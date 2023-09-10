bbf81b4874f2af3b395ce5b3b78d758becad0c32
mplanetganesh <mplanet.ganesh@gmail.com>
2023-09-10 20:17:45 +0530

    online course tutorials add changes

---
+++ python_course_1/day_2.py
@@ -0,0 +1,10 @@
+name = "Ram"
+_number = 10
+number2 = 2.3
+
+first_name_2 = "Shyam"
+
+print("Hello " + name)
+print(_number)
+print(first_name_2)
+print(number2)
---
+++ python_tutorials/datatype/list_comprehension.py
@@ -0,0 +1,3 @@
+# List Comprehension
+# Syntax
+# new_list = [expression for item in iterable if condition == True]
